29dc688d07bcad2fc811c15fd88a93eed99a02a4
Srinivas Girigowda <sgirigow@codeaurora.org>
2017-12-18 21:04:27 -0800

    qcacld-3.0: WLAN upgrade to 5.1.1.41W
    qcacld-3.0: Check kernel scheduler APIs for wake up on idle CPU
    66cld-3.0: Add support for SRD channels in ETSI domain
    4c2cfd3 qcacld-3.0: Support NDP Confirm with channel info and Schedule Update
    9c156a0 Release 5.1.1.41W
    a797d98 qcacld-3.0: Change the name of the sysfs path
    8fe4b84 qcacld-3.0: Print the src of country code
    b5e0653 Release 5.1.1.41V
    
    0221a65 qcacmn: Fix HTC packet map/unmap disparity
    d68e142 qcacmn: Add nbuf map/unmap tracking
    a2a85b2 qcacmn: Implement interface to set WLM level
    de9e290 qcacmn: Add debug support for DMA memory allocations
    4acc0b6 qcacmn: Fix double dmap problem in htc
    b733fb9 qcacmn: enqueue back dequeued packet if mmap fails
    38f5019 qcacmn: Debug print rate limiting
    b23ecea qcacmn: Change the data type of message type id
    
    7202838 fw-api: CL 3880548 - update fw common interface files
    e64ff43 fw-api: CL 3865355 - update fw common interface files
    f218fee fw-api: CL 3850357 - update fw common interface files
    0780d02 fw-api: CL 3833088 - update fw common interface files
    317f9b4 fw-api: CL 3804270 - update fw common interface files
    
    Bug: 70818178
    Change-Id: Id4ae5edd4d91bfbc0506db60f7eb1c1e63dc6f41
    Signed-off-by: Srinivas Girigowda <sgirigow@codeaurora.org>

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -41,9 +41,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            1
 #define QWLAN_VERSION_PATCH            1
-#define QWLAN_VERSION_EXTRA            "X"
-#define QWLAN_VERSION_BUILD            38
+#define QWLAN_VERSION_EXTRA            "W"
+#define QWLAN_VERSION_BUILD            41
 
-#define QWLAN_VERSIONSTR               "5.1.1.38X"
+#define QWLAN_VERSIONSTR               "5.1.1.41W"
 
 #endif /* QWLAN_VERSION_H */
