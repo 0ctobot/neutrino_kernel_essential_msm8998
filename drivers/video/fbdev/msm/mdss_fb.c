ee26ba0e893193e9f105ac193be06191f49518ff
Yaroslav Furman <yaro330@gmail.com>
2018-05-28 19:02:15 +0300

    mdss: turn backlight dimmer on by default
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    [@0ctobot: Fix invalid permissions]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -85,8 +85,8 @@
 #define MDSS_BRIGHT_TO_BL_DIM(out, v) do {\
 			out = ((v) * (v) * 255  / 4095 + (v) * (255 - (v)) / 32);\
 			} while (0)
-bool backlight_dimmer = false;
-module_param(backlight_dimmer, bool, 0755);
+bool backlight_dimmer = true;
+module_param(backlight_dimmer, bool, 0644);
 
 static struct fb_info *fbi_list[MAX_FBI_LIST];
 static int fbi_list_index;
