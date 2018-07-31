beaba71f6ad8ee55e8e6eee72f48d3f52e678f3e
Yaroslav Furman <yaro330@gmail.com>
2018-07-31 13:35:50 +0300

    msm-poweroff: always perform warm reboot
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -308,6 +308,7 @@ static void msm_restart_prepare(const char *cmd)
 	}
 #endif /* CONFIG_ESSENTIAL_APR */
 
+	need_warm_reset = true;
 	/* Hard reset the PMIC unless memory contents must be maintained. */
 	if (need_warm_reset) {
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
