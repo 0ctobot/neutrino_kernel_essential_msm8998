0e879bf4b8ced5852ff539472b00394c8f4ecaf0
Yaroslav Furman <yaro330@gmail.com>
2018-08-28 13:09:06 +0300

    tfa9891: fix enum conversion warnings
    ../sound/soc/codecs/tfa9891/tfa_dsp.c:3064:9: warning: implicit conversion from
    enumeration
          type 'enum Tfa98xx_Error' to different enumeration type 'enum tfa_error'
          [-Wenum-conversion]
            return err;
            ~~~~~~ ^~~
    ../sound/soc/codecs/tfa9891/tfa_dsp.c:3100:9: warning: implicit conversion from
    enumeration
          type 'enum Tfa98xx_Error' to different enumeration type 'enum tfa_error'
          [-Wenum-conversion]
            return err;
            ~~~~~~ ^~~
    ../sound/soc/codecs/tfa9891/tfa_dsp.c:3152:9: warning: implicit conversion from
    enumeration
          type 'enum Tfa98xx_Error' to different enumeration type 'enum tfa_error'
          [-Wenum-conversion]
            return err;
            ~~~~~~ ^~~
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- sound/soc/codecs/tfa9891/tfa98xx.c
+++ sound/soc/codecs/tfa9891/tfa98xx.c
@@ -118,9 +118,9 @@ static struct tfa98xx_rate rate_to_fssel[] = {
 };
 
 /* Wrapper for tfa start */
-static enum tfa_error tfa98xx_tfa_start(struct tfa98xx *tfa98xx, int next_profile, int *vstep)
+static enum Tfa98xx_Error tfa98xx_tfa_start(struct tfa98xx *tfa98xx, int next_profile, int *vstep)
 {
-	enum tfa_error err;
+	enum Tfa98xx_Error err;
 
 	err = tfa_start(next_profile, vstep);
 
@@ -725,7 +725,7 @@ static ssize_t tfa98xx_dbgfs_dsp_state_set(struct file *file,
 {
 	struct i2c_client *i2c = file->private_data;
 	struct tfa98xx *tfa98xx = i2c_get_clientdata(i2c);
-	enum tfa_error ret;
+	enum Tfa98xx_Error ret;
 	char buf[32];
 	const char start_cmd[] = "start";
 	const char stop_cmd[] = "stop";
